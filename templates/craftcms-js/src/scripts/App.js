23e555ba59c37d1dd294629d163298daff6a30bf
Chris Lusk <chris.m.lusk@gmail.com>
2023-02-23 10:41:40 -0600

    Have App handle the dynamic loading

--- templates/craftcms-js/src/scripts/App.js
+++ templates/craftcms-js/src/scripts/App.js
@@ -1,39 +1,91 @@
-import getModuleOptions from './utilities/getModuleOptions';
-import ModuleManifest from './ModuleManifest';
-
 export default class App {
   static moduleAttribute = 'data-module';
   static optionsAttribute = 'data-module-options';
+
+  static defaults = {
+    DynamicModules: [],
+    InitialModules: {},
+    scope: document.documentElement,
+  };
+
   /**
-   * @param {HTMLElement} scope
-   * @param {Object} config
+   * @param {Object} options
+   */
+  constructor(options = {}) {
+    this.options = { ...App.defaults, ...options };
+    this.init();
+  }
+
+  /**
    * @returns {App}
    */
-  constructor(scope = document.documentElement, config = {}) {
-    this.config = { ...App.defaults, ...config };
-    this.registerModules(scope);
-
+  init() {
+    this.registerModules();
     return this;
   }
 
   /**
-   * Loop over all modules in the defined scope, get any option values,
-   * and initialize each module if it exists in the `ModuleManifest` object.
+   * Loop over all elements with a module attribute, get any option values,
+   * and initialize each module if it is found in an existing manifest.
    *
-   * @param {HTMLElement} scope
-   * @returns void
+   * @returns {void}
    */
-  registerModules(scope) {
-    const modules = scope.querySelectorAll(`[${App.moduleAttribute}]`);
+  registerModules() {
+    const allElements = this.options.scope.querySelectorAll(`[${App.moduleAttribute}]`);
 
-    modules.forEach(module => {
-      const name = module.getAttribute(App.moduleAttribute);
-      if (!ModuleManifest[name]) {
+    allElements.forEach((element) => {
+      const name = element.getAttribute(App.moduleAttribute);
+
+      // Initial modules are already imported and can be initialized now.
+      if (this.options.InitialModules[name]) {
+        this.initModule(this.options.InitialModules[name], element);
         return;
       }
 
-      const Constructor = ModuleManifest[name];
-      new Constructor(module, getModuleOptions(module));
+      // If this module is in the dynamic manifest, import and initialize it.
+      if (this.options.DynamicModules.includes(name)) {
+        import(`./modules/${name}`)
+          .then(({ default: Module }) => {
+            this.initModule(Module, element);
+          })
+          .catch(error => {
+            console.error(`Error importing module "${name}"`, { error });
+          });
+      }
     });
+  }
+
+  /**
+   * @param {Function} Constructor
+   * @param {Element} element
+   * @returns {void}
+   */
+  initModule(Constructor, element) {
+    const options = this.getModuleOptions(element);
+    new Constructor(element, options);
+  }
+
+  /**
+   * @param {Element} element
+   * @returns {Object}
+   */
+  getModuleOptions(element) {
+    let options = {};
+
+    if (element.hasAttribute(App.optionsAttribute)) {
+      const optionString = element.getAttribute(App.optionsAttribute);
+
+      try {
+        options = JSON.parse(optionString);
+      } catch (error) {
+        const name = element.getAttribute(App.moduleAttribute);
+        console.error(`Error parsing module options for "${name}"`, {
+          error,
+          options: optionString,
+        });
+      }
+    }
+
+    return options;
   }
 }
